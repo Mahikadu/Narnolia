bd4977380fd0171343bf1769019c1d6ebd01005c
aniketadsul1 <aniketadsul1@gmail.com>
2016-12-05 16:30:17 +0530

    Resolved all test cases

---
+++ app/src/main/java/com/narnolia/app/multispinner/MultiSpinnerListener.java
@@ -0,0 +1,5 @@
+package com.narnolia.app.multispinner;
+
+public interface MultiSpinnerListener {
+    void onItemsSelected(boolean[] selected);
+}
---
+++ app/src/main/java/com/narnolia/app/multispinner/SpinnerListener.java
@@ -0,0 +1,7 @@
+package com.narnolia.app.multispinner;
+
+import java.util.List;
+
+public interface SpinnerListener {
+    void onItemsSelected(List<KeyPairBoolData> items);
+}
